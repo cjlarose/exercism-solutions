f2a9f270af2478dce2541d52cd0dbb8440ee4c5b
Chris LaRose <cjlarose@gmail.com>
2017-12-28 23:50:15 -0800

    Formatting

--- haskell/rail-fence-cipher/src/RailFenceCipher.hs
+++ haskell/rail-fence-cipher/src/RailFenceCipher.hs
@@ -3,7 +3,9 @@ module RailFenceCipher (encode, decode) where
 import Data.List (sortOn, sort)
 
 encode :: Int -> [a] -> [a]
-encode n = map snd . sortOn fst . zip (cycle $ [0..(n-1)] ++ [n-2,n-3..1])
+encode n = map snd . sortOn fst . zip p
+  where p = cycle $ [0..(n-1)] ++ [n-2,n-3..1]
 
 decode :: Int -> String -> String
-decode n xs = map snd . sort . zip (encode n [0..(length xs-1)]) $ xs
+decode n xs = map snd . sort . zip ii $ xs
+  where ii = encode n [0..(length xs-1)]
